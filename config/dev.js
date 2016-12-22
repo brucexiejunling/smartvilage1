63d1c53f6b6796c5745f530f73698531fee81ea7
逍龙 <junling.xjl@alibaba-inc.com>
2016-12-22 16:09:21 +0800

    rebuild

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
     env: 'development', //环境名称
     hostname: 'www.zmwhzhizhui.com',
-    port: 80,         //服务端口号
+    port: 3000,         //服务端口号
     mongodb_url: '',    //数据库地址
     redis_url:'',       //redis地址
     redis_port: ''      //redis端口号
